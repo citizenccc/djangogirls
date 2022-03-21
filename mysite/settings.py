c1c51ed2b5329abf8248bfb7265878f3c2fe90da
citizenccc <v.pisarev69@gmail.com>
2022-03-21 14:15:33 +0600

    new djangogirls

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'djangogirls_db',
-        'USER': 'postgres',
-        'PASSWORD': '1',
-        'HOST': 'localhost',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
